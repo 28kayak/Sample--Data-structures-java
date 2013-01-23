f5648ac35373680f5962ab3405ba77a3cb4dc8c3
kayak28 <kaya_ota@msn.com>
2013-01-23 11:10:30 -0800

    create a javafile to abstract TestMethod

---
+++ MethodTest.java
@@ -0,0 +1,25 @@
+/*
+ * To test various classes
+ */
+
+
+
+
+import java.util.Scanner;
+
+
+
+public class MethodTest
+{
+	public static void main(String [] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+	}
+	
+	
+	
+	
+	
+	
+}//class
