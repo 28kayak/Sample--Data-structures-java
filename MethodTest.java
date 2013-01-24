72302735e4510de8311d6557c13f0f296b6ce986
kayak28 <kaya_ota@msn.com>
2013-01-23 20:03:17 -0800

    create a method to test multiple classes

--- MethodTest.java
+++ MethodTest.java
@@ -14,6 +14,19 @@ public class MethodTest
 	public static void main(String [] args)
 	{
 		Scanner scan = new Scanner(System.in);
+		String insufficiency; 
+		int allocateSeat;
+		 
+		System.out.println("Are 10 seats enough for your seat-chart? Y or N");
+		insufficiency = scan.nextLine();
+		if(insufficiency.equalsIgnoreCase("N"))
+		{
+			System.out.println("How many seat do you need??");
+			allocateSeat = scan.nextInt();
+			//SeatChart seatChart = new SeatChart(allocateSeat);
+			System.out.println(allocateSeat);
+			
+		}		
 		
 	}
 	
